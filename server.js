d242dc93349d3390e7dc695de7153ebf50fd4226
unknown <Buzz-brain>
2024-12-26 21:15:20 +0100

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs'); 
 app.set("views", path.join(__dirname, "views"));
 
